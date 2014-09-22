2d37e1a72a6ad3268203d44a0ccbe72bfeaf0647
Adam Bednarski <abednarski79@gmail.com>
2014-09-22 23:30:25 +0100

    shared lib added

--- src/eu/appbucket/beaconmonitor/ui/MainActivity.java
+++ src/eu/appbucket/beaconmonitor/ui/MainActivity.java
@@ -2,7 +2,6 @@ package eu.appbucket.beaconmonitor.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import eu.appbucket.beaconmonitor.R;
